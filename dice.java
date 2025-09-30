dbcb1b814063888e013fd21b93292d6fb1ecce2a
Shriya-26-Sharma <shriyasharma0126@gmail.com>
2025-09-30 22:40:59 +0530

    Rolling A Dice

---
+++ dice.java
@@ -0,0 +1,12 @@
+class dice{
+    int roll(){
+      double random = Math.random()*6;
+      return(int)Math.ceil(random);
+    }
+    public static void main(String[] args) {
+        dice ob= new dice();
+        for (int i=0;i<10;i++){
+        System.out.println(ob.roll());
+        }
+    }
+}
